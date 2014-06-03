23cbd50dbeb76a462c0085a5f8c33715e75cb6dd
nightlinus <m.a.ogarkov@gmail.com>
2014-06-03 16:20:17 +0400

    Initial commit

---
+++ src/Exception.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Date: 03.06.14
+ * Time: 16:19
+ *
+ * @category
+ * @package  OracleMeta
+ * @author   nightlinus <user@localhost>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version
+ * @link
+ */
+
+namespace nightlinus\OracleMeta;
+
+
+class Exception extends \Exception
+{
+
+}
